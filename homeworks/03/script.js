2c9cd76f31cf0547165a71310b004fceff49873c
Mario Petek <mariopetek0@gmail.com>
2024-03-21 17:17:06 +0100

    Add question container with question and answers
    Once the quiz has started, the question container appears with the
    question and four answers. The container is styled using colors and
    other properties similar to the starting container. When an answer is
    selected, the container appears with an outline to indicate that the
    answer is selected.

--- homeworks/03/script.js
+++ homeworks/03/script.js
@@ -11,8 +11,9 @@ async function startQuiz() {
     startContainer.parentNode.removeChild(startContainer)
     startButton.removeEventListener('click', startQuiz)
 
-    const question = await getQuestionData()
-    console.log(question)
+    const questionData = await getQuestionData()
+    console.log(questionData)
+    showQuestionContainer(questionData)
 }
 
 async function getQuestionData() {
@@ -34,8 +35,14 @@ async function getQuestionData() {
                 'An error occurred while fetching the question. Please try again.'
             )
         }
-        const questionData = await response.json()
-        return questionData.results[0]
+        const data = await response.json()
+        const questionData = data.results[0]
+        return {
+            question: questionData.question,
+            difficulty: questionData.difficulty,
+            correctAnswer: questionData.correct_answer,
+            incorrectAnswers: questionData.incorrect_answers
+        }
     } catch (error) {
         showError(error.message)
     } finally {
@@ -61,4 +68,58 @@ function showError(message) {
     errorMessage.classList.add('error-message')
     errorMessage.innerHTML = `${message}`
     document.body.appendChild(errorMessage)
+}
+
+function showQuestionContainer(questionData) {
+    const questionNumber = document.createElement('h2')
+    questionNumber.innerText = `Question ${currentQuestion}`
+    const questionText = document.createElement('p')
+    questionText.innerHTML = questionData.question
+
+    const questionContent = document.createElement('div')
+    questionContent.classList.add('question-content')
+    questionContent.append(questionNumber, questionText)
+
+    const answersContainer = document.createElement('div')
+    answersContainer.classList.add('answers-container')
+    const answers = [
+        questionData.correctAnswer,
+        ...questionData.incorrectAnswers
+    ]
+    shuffle(answers)
+
+    answers.forEach((answer, index) => {
+        const answerInput = document.createElement('input')
+        answerInput.type = 'radio'
+        answerInput.name = 'answer'
+        answerInput.id = `answer${index + 1}`
+        answerInput.value = answer
+
+        const answerLabel = document.createElement('label')
+        answerLabel.htmlFor = `answer${index + 1}`
+        answerLabel.innerText = answer
+
+        const answerCard = document.createElement('div')
+        answerCard.classList.add('answer-card')
+        answerCard.append(answerInput, answerLabel)
+
+        answersContainer.appendChild(answerCard)
+    })
+
+    const answerButton = document.createElement('button')
+    answerButton.classList.add('answer-button')
+    answerButton.innerText = 'Answer'
+
+    const questionContainer = document.createElement('div')
+    questionContainer.classList.add('question-container')
+    questionContainer.append(questionContent, answersContainer, answerButton)
+
+    document.body.prepend(questionContainer)
+}
+
+function shuffle(answers) {
+    const randomIndex = Math.floor(Math.random() * answers.length)
+    const temp = answers[0]
+    answers[0] = answers[randomIndex]
+    answers[randomIndex] = temp
 }
