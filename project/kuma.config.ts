6a68e369440c04e211b27bec2ee5343e141d92a8
Mario Petek <mariopetek0@gmail.com>
2024-05-17 17:34:27 +0200

    Define theme colors

--- project/kuma.config.ts
+++ project/kuma.config.ts
@@ -1,25 +1,45 @@
-import { createTheme } from "@kuma-ui/core";
+import { createTheme } from '@kuma-ui/core'
 
 const theme = createTheme({
   colors: {
-    primary: 'var(--primary-default)',
+    primary: {
+      default: 'var(--primary-default)',
+      variant: 'var(--primary-variant)',
+      highlight: 'var(--primary-highlight)',
+    },
+    secondary: {
+      default: 'var(--secondary-default)',
+      variant: 'var(--secondary-variant)',
+      highlight: 'var(--secondary-highlight)',
+    },
+    onColor: {
+      primary: 'var(--on-color-primary)',
+      secondary: 'var(--on-color-secondary)',
+    },
     surface: {
-      s0: 'var(--surface.s0)',
-      s1: 'var(--surface.s1)',
+      s0: 'var(--surface-0)',
+      s1: 'var(--surface-1)',
+      s2: 'var(--surface-2)',
     },
     onSurface: {
-      nLv1: 'var(--on-surface-nLv1)',
-      nLv2: 'var(--on-surface-nLv2)',
-    }
+      lv1: 'var(--on-surface-lv1)',
+      lv2: 'var(--on-surface-lv2)',
+      lv3: 'var(--on-surface-lv3)',
+      lv4: 'var(--on-surface-lv4)',
+    },
+    error: 'var(--error)',
+    alert: 'var(--alert)',
+    success: 'var(--success)',
+    live: 'var(--live)',
   },
   spacings: {
-    sm: "8px",
-    md: "12px",
-    lg: "16px",
+    sm: '8px',
+    md: '12px',
+    lg: '16px',
   },
   breakpoints: {
-    sm: "400px",
-    md: "700px",
+    sm: '400px',
+    md: '700px',
   },
   fontWeights: {
     light: 300,
@@ -28,40 +48,18 @@ const theme = createTheme({
     bold: 700,
   },
   fontSizes: {
-    xs: "12px",
-    sm: "14px",
-    md: "16px",
-    lg: "20px",
-    xl: "24px",
+    xs: '12px',
+    sm: '14px',
+    md: '16px',
+    lg: '20px',
+    xl: '24px',
   },
-  components: {
-    Button: {
-      defaultProps: {
-        variant: "primary",
-        padding: '4px',
-        fontWeight: 600,
-        _hover: {
-          opacity: 0.9,
-        },
-      },
-      variants: {
-        primary: {
-          bg: "var(--primary-default)",
-          color: "var(--on-surface-nLv1)",
-        },
-        secondary: {
-          bg: "var(--on-surface-nLv1)",
-          color: "var(--primary-default)",
-        },
-      },
-    },
-  },
-});
+})
 
-type UserTheme = typeof theme;
+type UserTheme = typeof theme
 
-declare module "@kuma-ui/core" {
+declare module '@kuma-ui/core' {
   export interface Theme extends UserTheme {}
 }
 
-export default theme;
+export default theme
