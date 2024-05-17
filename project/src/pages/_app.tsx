eafeb6144e314cdbcf0bc65a9a3a4161c59cd27b
Mario Petek <mariopetek0@gmail.com>
2024-05-17 17:40:02 +0200

    Define Roboto font family

--- project/src/pages/_app.tsx
+++ project/src/pages/_app.tsx
@@ -2,11 +2,17 @@ import '@/styles/globals.css'
 import { ThemeContextProvider } from '@/context/ThemeContext'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+})
 
 //@ts-ignore
 export const fetcher = (...args) =>
   //@ts-ignore
-  fetch(...args).then((res) => {
+  fetch(...args).then(res => {
     if (res.ok) {
       return res.json()
     } else {
@@ -18,6 +24,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig value={{ fetcher }}>
       <ThemeContextProvider>
+        <style jsx global>{`
+          html {
+            font-family: ${roboto.style.fontFamily};
+          }
+        `}</style>
         <Component {...pageProps} />
       </ThemeContextProvider>
     </SWRConfig>
