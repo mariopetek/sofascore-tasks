c09779226f546acbb775f26312ea3c26a25df2d1
Mario Petek <mariopetek0@gmail.com>
2024-05-30 20:24:53 +0200

    Add settings button to header

--- project/src/modules/Header.tsx
+++ project/src/modules/Header.tsx
@@ -1,29 +1,45 @@
-import { getSports } from '@/api/sport'
 import { useThemeContext } from '@/context/ThemeContext'
 import { Box, Button } from '@kuma-ui/core'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const SPORTS_ICONS_URLS = {
-  football: '/icons/sports/icon_football.svg',
-  basketball: '/icons/sports/icon_basketball.svg',
-  'american-football': '/icons/sports/icon_american_football.svg',
-} as Record<string, string>
+const SPORTS = [
+  {
+    slug: 'football',
+    name: 'Football',
+    icon: '/icons/sports/icon_football.svg',
+  },
+  {
+    slug: 'basketball',
+    name: 'Basketball',
+    icon: '/icons/sports/icon_basketball.svg',
+  },
+  {
+    slug: 'american-football',
+    name: 'American Football',
+    icon: '/icons/sports/icon_american_football.svg',
+  },
+]
 
 export default function Header() {
   const { isDark, setIsDark } = useThemeContext()
 
   const router = useRouter()
-  const isActive = (href: string) => router.asPath === href
-
-  const { sports, sportsError } = getSports()
-
-  if (sportsError) return <Box>Error</Box>
-  if (!sports) return <Box>Loading...</Box>
+  const isActive = (href: string) => router.asPath.startsWith(href)
 
   return (
     <Box as="header" position="sticky" top={0} backgroundColor="colors.primary.default" zIndex={1}>
-      <Button onClick={() => setIsDark(v => !v)}>Theme</Button>
+      <Button
+        position="absolute"
+        top="spacings.lg"
+        right="spacings.xl"
+        maskSize="24px 24px"
+        maskImage="url(/icons/system/ic_settings.svg)"
+        backgroundColor="colors.surface.s1"
+        width="24px"
+        height="24px"
+        onClick={() => setIsDark(v => !v)}
+      ></Button>
       <Box height="64px" display="flex" justifyContent="center" alignItems="center">
         <Link href="/">
           <Box
@@ -36,8 +52,8 @@ export default function Header() {
         </Link>
       </Box>
       <Box height="48px" display="flex" justifyContent="center" gap="16px">
-        {sports.map(sport => (
-          <Link href={sport.slug} key={sport.id}>
+        {SPORTS.map(sport => (
+          <Link href={sport.slug} key={sport.slug}>
             <Box
               display="flex"
               alignItems="center"
@@ -49,7 +65,7 @@ export default function Header() {
             >
               <Box
                 maskSize="16px 16px"
-                maskImage={`url(${SPORTS_ICONS_URLS[sport.slug]})`}
+                maskImage={`url(${sport.icon})`}
                 backgroundColor="colors.surface.s1"
                 width="16px"
                 height="16px"
